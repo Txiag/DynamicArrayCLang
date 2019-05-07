5f161b22b4da4c5d317d4d8d45bb4517bc0695fe
Thiago Nascimento <42191501+Txiag@users.noreply.github.com>
2019-05-07 13:28:45 -0300

    Add files via upload

---
+++ array_list_int.c
@@ -0,0 +1,142 @@
+/**
+ * ANSI C Array List of int values library
+ *
+ * Implementation file
+ *
+ * Author: Thiago do Nascimento Ferreira
+ * Date: 20/05/2019
+ *
+ */
+
+
+#include "array_list_int.h"
+
+#include <stdlib.h> /* For malloc and free */
+
+#define MAGIC 314159265
+
+struct array_list_int{
+  int *a;
+  int size;
+  int capacity;
+  int magic;
+};
+
+/* Internal functions */
+
+/* Check if the pointer actually points to a valid array_list_int struc */
+int ali_check_type(array_list_int ali){
+  if (ali->magic!=MAGIC){
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+/* Increase capacity size of the array_list_int internal storage */
+int ali_realloc(array_list_int ali){
+  /* TODO: */
+  return 0; /* Realloc could not allocate new memory */
+}
+
+
+/* External (public) functions.
+ * + Defined in array_list_int.h file
+ */
+
+/* creates new instance of a dynamic array */
+array_list_int ali_create(){
+  array_list_int ali= (array_list_int)malloc(sizeof(struct array_list_int));
+  ali->a = (int*)malloc(sizeof(int)*4);
+  ali->size=0;
+  ali->capacity=4;
+  ali->magic=MAGIC;
+  return ali;
+}
+
+/* Get the value at index 'index' */
+int ali_get(array_list_int ali, int index){
+  if (!ali_check_type(ali))
+    return 0;
+  if (index<0 || index>=ali->size){
+    return 0;
+  }
+  return ali->a[index];
+}
+
+/* Adds value 'value' to the end of the dynamic array */
+unsigned int ali_push_back(array_list_int ali, int value){
+  if (!ali_check_type(ali))
+    return 0;
+  if (ali->size == ali->capacity)
+    if (!ali_realloc(ali))
+      return ali->size;
+  ali->a[ali->size++]=value;
+  return ali->size;
+}
+
+/* Removes last value of dynamic array 'ali'
+ *
+ */
+unsigned int ali_pop_back(array_list_int ali){
+  if (!ali_check_type(ali))
+    return 0;
+if (ali->size == 0)
+    return 0;
+  return --(ali->size);
+}
+
+
+/* Gets number of int elements stored in 'ali' */
+unsigned int ali_size(array_list_int ali){
+  if (!ali_check_type(ali))
+    return 0;
+  return ali->size;
+}
+
+
+int ali_find(array_list_int ali, int value){
+  int i = 0;
+  for (i=0; i<ali.size; i++){
+    if (ali.a[i] == value){
+      return i;
+    }
+  }
+  return -1;
+}
+
+/**
+ * TODO:
+ */
+int ali_insert_at(array_list_int ali, int index, int value){
+  
+  return 0;
+}
+
+/**
+ * TODO:
+ */
+int ali_remove_from(array_list_int ali, int index){
+  return ali->size;
+}
+
+/**
+ * TODO: */
+unsigned int ali_capacity(array_list_int ali){
+  return 0;
+}
+
+
+/**
+ * TODO: */
+double ali_percent_occuped(array_list_int ali){
+  return 0.0;
+}
+
+/* Release memory used by the struct and invalidate it. */
+void ali_destroy(array_list_int ali){
+  free(ali->a);
+  ali->size=0;
+  ali->capacity=0;
+  ali->magic=0;
+}
---
+++ teste_desempenho_exemplo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include "array_list_int.h"
+
+
+/* calcula o tempo corrido em nanosegundos */
+unsigned long long dif_time(struct timespec begin, struct timespec end){
+  struct timespec dif;
+  if (end.tv_nsec<=begin.tv_nsec){
+    end.tv_nsec+=1000000000;
+    end.tv_sec-=1;
+  }
+  dif.tv_sec = end.tv_sec - begin.tv_sec;
+  dif.tv_nsec = end.tv_nsec - begin.tv_nsec;
+  return dif.tv_sec*1000000000 + dif.tv_nsec;
+}
+
+int main(){
+  int i;
+  array_list_int a1 = ali_create();
+  struct timespec begin,end;
+
+  clock_gettime(CLOCK_REALTIME, &begin);
+  for (i=0 ; i<10000 ; ++i){
+    ali_push_back(a1,i);
+  }
+  clock_gettime(CLOCK_REALTIME, &end);
+
+  printf("Tempo para inserir: %lluns\n",dif_time(begin,end));
+  ali_destroy(a1);
+  return 0;
+}
